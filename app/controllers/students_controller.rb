cb93ca25a883118215d036529c3f085e5e08f654
Vincent Pham <40014512+phamdvincent@users.noreply.github.com>
2024-02-09 20:46:26 +0000

    Completed debugging

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,6 @@
 class StudentsController < ApplicationController
   def index
-    @students = Student.all.order({ :created_at => :desc })
+    @students = Student.all.order({ :created_at => :asc })
 
     render({ :template => "students/index" })
   end
